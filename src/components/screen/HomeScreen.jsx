5d67be04ddeffd28478761cfd972f105e2660df0
farnoodlotfali <farnoodlotfali@gmail.com>
2024-11-11 14:33:57 +0330

    up4

--- src/components/screen/HomeScreen.jsx
+++ src/components/screen/HomeScreen.jsx
@@ -17,7 +17,13 @@ import { EndPoints } from "@/constants/EndPoints";
 import { axiosApi } from "@/api/axiosApi";
 import { useState } from "react";
 import { enToFaNumber } from "@/utility/utils";
-import { Scanner } from "@yudiel/react-qr-scanner";
+import {
+  boundingBox,
+  centerText,
+  outline,
+  Scanner,
+  useDevices,
+} from "@yudiel/react-qr-scanner";
 
 const InfoItem = ({ title, info }) => {
   return (
@@ -30,9 +36,40 @@ const InfoItem = ({ title, info }) => {
   );
 };
 
+const styles = {
+  container: {
+    width: 400,
+    margin: "auto",
+  },
+  controls: {
+    marginBottom: 8,
+  },
+};
+
 const HomeScreen = () => {
   const [data, setData] = useState("No result");
   const [info, setInfo] = useState(null);
+
+  const [deviceId, setDeviceId] = useState(undefined);
+  const [tracker, setTracker] = useState("centerText");
+
+  const [pause, setPause] = useState(false);
+
+  const devices = useDevices();
+
+  function getTracker() {
+    switch (tracker) {
+      case "outline":
+        return outline;
+      case "boundingBox":
+        return boundingBox;
+      case "centerText":
+        return centerText;
+      default:
+        return undefined;
+    }
+  }
+
   const {
     handleSubmit,
     formState: { errors, isSubmitting },
@@ -79,7 +116,77 @@ const HomeScreen = () => {
       }}
     >
       <Box>
-        <BarcodeScanner Scanner onScan={(result) => setData(result)} />
+        <button
+          style={{ marginBottom: 5 }}
+          onClick={() => setPause((val) => !val)}
+        >
+          {pause ? "Pause Off" : "Pause On"}
+        </button>{" "}
+        <div style={styles.controls}>
+          <select onChange={(e) => setDeviceId(e.target.value)}>
+            <option value={undefined}>Select a device</option>
+            {devices.map((device, index) => (
+              <option key={index} value={device.deviceId}>
+                {device.label}
+              </option>
+            ))}
+          </select>
+          <select
+            style={{ marginLeft: 5 }}
+            onChange={(e) => setTracker(e.target.value)}
+          >
+            <option value="centerText">Center Text</option>
+            <option value="outline">Outline</option>
+            <option value="boundingBox">Bounding Box</option>
+            <option value={undefined}>No Tracker</option>
+          </select>
+        </div>
+        <Scanner
+          formats={[
+            "qr_code",
+            "micro_qr_code",
+            "rm_qr_code",
+            "maxi_code",
+            "pdf417",
+            "aztec",
+            "data_matrix",
+            "matrix_codes",
+            "dx_film_edge",
+            "databar",
+            "databar_expanded",
+            "codabar",
+            "code_39",
+            "code_93",
+            "code_128",
+            "ean_8",
+            "ean_13",
+            "itf",
+            "linear_codes",
+            "upc_a",
+            "upc_e",
+          ]}
+          // onScan={(result) => setData(result)}
+          onScan={(detectedCodes) => {
+            setData(detectedCodes);
+          }}
+          constraints={{
+            deviceId: deviceId,
+          }}
+          components={{
+            audio: true,
+            onOff: true,
+            torch: true,
+            zoom: true,
+            finder: true,
+            tracker: getTracker(),
+          }}
+          onError={(error) => {
+            console.log(`onError: ${error}'`);
+          }}
+          allowMultiple={true}
+          scanDelay={2000}
+          // paused={pause}
+        />
         <p>{data}</p>
       </Box>
 
